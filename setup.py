606ece1efb0833441280e2f1416e0a23fe6eef65
Michael Howitz <mh@gocept.com>
2009-05-28 19:03:17 +0000

    Preparing release 1.1.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.4dev',
+version = '1.1.0'
 
 setup(
     name='z3c.pagelet',
