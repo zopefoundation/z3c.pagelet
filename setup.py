5494d8e4ee75632dad0ffe3a9f44ce172a94eb87
Dan Korostelev <nadako@gmail.com>
2009-08-27 16:34:47 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.0dev'
+version = '1.2.1dev'
 
 setup(
     name='z3c.pagelet',
