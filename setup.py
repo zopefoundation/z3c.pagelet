6d6e72450d0cbbb2888adf32219f6adba73cd2d3
Roger Ineichen <roger@projekt01.ch>
2011-08-18 11:59:11 +0000

    prepare release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.2dev'
+version = '1.2.2'
 
 setup(
     name='z3c.pagelet',
