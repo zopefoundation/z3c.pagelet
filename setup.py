45c705e15e482deb06d930f0fc6deb576f478dd3
Roger Ineichen <roger@projekt01.ch>
2011-10-29 21:25:10 +0000

    back to dev

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.3.0'
+version = '1.3.1dev'
 
 setup(
     name='z3c.pagelet',
